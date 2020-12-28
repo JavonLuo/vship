a2a7b18a3c483c344ad94df83b6cfe6901b2b569
luojiafeng <v_jiafluo@tencent.com>
2020-12-28 16:34:31 +0800

    提交代码

--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -1,4 +1,3 @@
-import { type } from 'os'
 import React from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
