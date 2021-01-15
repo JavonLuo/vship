e587fd45b8701fb533516037cdda740f32e41691
luojiafeng <v_jiafluo@tencent.com>
2021-01-15 18:25:38 +0800

    Select

--- src/components/Select/option.tsx
+++ src/components/Select/option.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import classNames from 'classnames'
 
-export const Option: React.FC = () => {
+export interface OptionProps {
+  value?: string;
+  disabled?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const Option: React.FC<OptionProps> = (props) => {
+  const { value, disabled, className, style, children } = props
+  // const context = useContext(MenuContext)
+  const classes = classNames('vship-select-dropdown-menu-item', className, {
+    'vship-select-dropdown-menu-item-disabled': disabled,
+    // 'is-active': context.index === index && !disabled
+  })
+  // const handleClick = () => {
+  //   if (context.onSelect && !disabled && (typeof index === 'string')) {
+  //     context.onSelect(index)
+  //   }
+  // }
   return (
-    <span>ddd</span>
+    <li className={classes} style={style}>
+      {children}
+    </li>
   )
 }
-export default Option;
+Option.displayName = 'Option'
+
+export default Option
--- src/components/Select/select.tsx
+++ src/components/Select/select.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import Icon from '../Icon/icon'
+import { OptionProps } from './option'
 export interface SelectProps {
   defaultValue?: string
 }
 
+// export const MenuContext = createContext<SelectProps>()
 export const Select: React.FC<SelectProps> = (props) => {
+  const { children } = props
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<OptionProps>
+      const { displayName } = childElement.type
+      if (displayName === 'Option') {
+        return React.cloneElement(childElement, {
+          // index: index.toString()
+        })
+      } else {
+        console.error('warning: Select has a child which is not a Option component')
+      }
+    })
+  }
   return (
-    <div className='vship-select' >
+    <div className='vship-select'>
       <div className='vship-select-selection vship-select-selection--single'>
         <div className='vship-select-selection__rendered'>Jack</div>
         <span className='vship-select-arrow'>
           <Icon icon='angle-down' />
         </span>
+      </div>
+      <div className='vship-select-dropdown'>
+        <ul className='vship-select-dropdown-menu'>
+          {renderChildren()}
+        </ul>
       </div>
     </div>
   )
