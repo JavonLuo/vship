d1b1f941cf42352d7f1c1b56e95acf72c2227969
luojiafeng <v_jiafluo@tencent.com>
2021-01-19 19:00:43 +0800

    select component

--- src/components/Select/option.tsx
+++ src/components/Select/option.tsx
@@ -1,27 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, MouseEvent } from 'react'
 import classNames from 'classnames'
-
+import { SelectContext } from './select'
 export interface OptionProps {
-  value?: string;
+  value: string | number;
   disabled?: boolean;
   className?: string;
+  index?: number;
   style?: React.CSSProperties;
 }
 
 const Option: React.FC<OptionProps> = (props) => {
-  const { value, disabled, className, style, children } = props
-  // const context = useContext(MenuContext)
-  const classes = classNames('vship-select-dropdown-menu-item', className, {
-    'vship-select-dropdown-menu-item-disabled': disabled,
-    // 'is-active': context.index === index && !disabled
+  const { value, disabled, className, style, children, index } = props
+  const context = useContext(SelectContext)
+  const cnames = classNames('vship-select-option-item', className, {
+    'vship-select-dropdown-item-disabled': disabled,
+    'is-active': context.index === index
   })
-  // const handleClick = () => {
-  //   if (context.onSelect && !disabled && (typeof index === 'string')) {
-  //     context.onSelect(index)
-  //   }
-  // }
+  const handleClick = (e: MouseEvent<HTMLElement>, value: any, index: any) => {
+    e.stopPropagation()
+    const onSelect = context.onSelect as Function
+    if (!disabled) {
+      onSelect(value, index)
+    }
+  }
   return (
-    <li className={classes} style={style}>
+    <li className={cnames} style={style} onClick={(e) => {handleClick(e, value, index)}}>
       {children}
     </li>
   )
--- src/components/Select/select.tsx
+++ src/components/Select/select.tsx
@@ -1,41 +1,151 @@
-import React, { createContext } from 'react'
+import React, { createContext, KeyboardEvent, useState, useRef } from 'react'
+import classNames from 'classnames'
 import Icon from '../Icon/icon'
+import Empty from '../Empty/empty'
+import Transition from '../Transition/transition'
 import { OptionProps } from './option'
+import useClickOutside from '../../Hooks/useClickOutside'
+
+type SelectSize = 'large' | 'middle' | 'small'
 export interface SelectProps {
-  defaultValue?: string
+  size?: SelectSize
+  onSelect?: (value: string) => void
+  defaultValue?: string | number
+  placeholder?: string
 }
 
-// export const MenuContext = createContext<SelectProps>()
+interface ISelectContext {
+  index: number;
+  onSelect?: (value: string, index: number) => void;
+}
+export const SelectContext = createContext<ISelectContext>({ index: -1 })
 export const Select: React.FC<SelectProps> = (props) => {
+  const {
+    size,
+    onSelect,
+    defaultValue,
+    placeholder,
+    ...restProps
+  } = props
   const { children } = props
+  const componentRef = useRef<HTMLDivElement>(null)
+  const [highlightIndex, setHighlightIndex] = useState<number>(-1)
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
+  const [selectedValue, setSelectedValue] = useState(defaultValue)
+  const [focus, setFocus] = useState<boolean>(false)
+  useClickOutside(componentRef, () => { setFocus(false); setShowDropdown(false) })
+  const handleKeyDown = (e: KeyboardEvent<HTMLUListElement>) => {
+    switch (e.keyCode) {
+      case 13:
+        // if (suggestions[highlightIndex]) {
+        //   handleSelect(suggestions[highlightIndex])
+        // }
+        break;
+      case 38:
+        highlight('minus')
+        break;
+      case 40:
+        highlight('plus')
+        break
+      case 27:
+        // setShowDropdown(false)
+        break;
+      default:
+        break;
+    }
+  }
+  const highlight = (action: string) => {
+    const children = renderChildren()
+    let activeIndex = highlightIndex
+    if (action === 'minus') {
+      activeIndex = highlightIndex -1
+    } else if(action === 'plus') {
+      activeIndex = highlightIndex + 1
+    }
+    if (children) {
+      if (activeIndex < 0) {
+        activeIndex = children.length - 1
+      } else if (activeIndex > children.length -1) {
+        activeIndex = 0
+      }
+      while (children[activeIndex].props.disabled) {
+        if (highlightIndex > activeIndex) {
+          activeIndex = activeIndex - 1
+        } else {
+          activeIndex = activeIndex + 1
+        }
+      }
+    } 
+    setHighlightIndex(activeIndex)
+  }
   const renderChildren = () => {
-    return React.Children.map(children, (child, index) => {
+    const _children = React.Children.map(children, (child, index) => {
       const childElement = child as React.FunctionComponentElement<OptionProps>
       const { displayName } = childElement.type
       if (displayName === 'Option') {
         return React.cloneElement(childElement, {
-          // index: index.toString()
+          index: index
         })
       } else {
         console.error('warning: Select has a child which is not a Option component')
       }
     })
+    return _children
   }
+  const handleClick = (value: string, index: number) => {
+    // setHighlightIndex(index)
+    setShowDropdown(false)
+    setSelectedValue(value)
+    if (onSelect) {
+      onSelect(value)
+    }
+  }
+  const handleFocus = () => {
+    setFocus(true)
+    setShowDropdown(true)
+  }
+  const passedContext: ISelectContext = {
+    index: highlightIndex,
+    onSelect: handleClick
+  }
+  const cnames = classNames('vship-select-wrapper', {
+    [`select-size-${size}`]: size
+  })
+  const klass = classNames('vship-select-inner', {
+    'vship-select-inner-focus': focus,
+    'vship-select-inner-after': !placeholder && !selectedValue,
+    'vship-select-inner-placeholder': !selectedValue
+  })
   return (
-    <div className='vship-select'>
-      <div className='vship-select-selection vship-select-selection--single'>
-        <div className='vship-select-selection__rendered'>Jack</div>
+    <div className={cnames} {...restProps} ref={componentRef} onClick={handleFocus}>
+      <div className={klass}>
+        <div>{selectedValue ? selectedValue : placeholder}</div>
         <span className='vship-select-arrow'>
-          <Icon icon='angle-down' />
+          <Icon icon='angle-down' className={showDropdown ? 'icon-down icon-down-focus' : 'icon-down'} />
         </span>
       </div>
-      <div className='vship-select-dropdown'>
-        <ul className='vship-select-dropdown-menu'>
-          {renderChildren()}
+      <Transition
+        in={showDropdown}
+        animation='zoom-in-top'
+        timeout={300}
+        onExited={() => {}}
+      >
+        <ul className='vship-option-list'>
+          <SelectContext.Provider value={passedContext}>
+            {renderChildren() ?
+              renderChildren() :
+              <div className='vship-option-empty'>
+                <Empty />
+              </div>
+            }
+          </SelectContext.Provider>
         </ul>
-      </div>
+      </Transition>
     </div>
   )
+}
+Select.defaultProps = {
+  size: 'middle'
 }
 
 export default Select;
